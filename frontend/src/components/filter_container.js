86e78dbdadc5dc11f3b4632476c2dbbb69d5c115
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-16 15:50:10 -0400

    starting to add logic for the filters

--- frontend/src/components/filter_container.js
+++ frontend/src/components/filter_container.js
@@ -1,21 +1,45 @@
 // filter_container.js
-import React from 'react';
+import React, { Component } from 'react';
 
 import DateFilter from './date_filter';
 import TimeFilter from './time_filter';
 import CategoryFilter from './category_filter';
 import ApplyFilterButton from './apply_filter_button';
 
-const FilterContainer = (props) => {
-  return (
-    <div id="filter-container">
-      <p>I am the container that holds all the filter options.</p>
-      <DateFilter />
-      <TimeFilter />
-      <CategoryFilter />
-      <ApplyFilterButton />
-    </div>
-  );
-};
+class FilterContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDate: null,
+      selectedTime: null,
+      selectedCategories: [],
+    };
+  }
+  onDateChange(event) {
+    this.setState({ selectedDate: event.target.value });
+  }
+  onTimeChange(event) {
+    this.setState({ selectedTime: event.target.value });
+  }
+  onCategoryChange(event) {
+    this.setState({ selectedCategories: event.target.value });
+  }
+  applyFilters(event) {
+    console.log('applying the filters:');
+    console.log(this.state);
+    this.props.onApplyFilter(this.state);
+  }
+  render() {
+    return (
+      <div id="filter-container">
+        <p>I am the container that holds all the filter options.</p>
+        <DateFilter onDateChange={this.onDateChange} />
+        <TimeFilter onTimeChange={this.onTimeChange} />
+        <CategoryFilter onCategoryChange={this.onCategoryChange} />
+        <ApplyFilterButton applyFilters={this.applyFilters} />
+      </div>
+    );
+  }
+}
 
 export default FilterContainer;
