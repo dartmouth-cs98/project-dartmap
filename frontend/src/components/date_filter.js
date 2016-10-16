9bf011e3e9d27babb8da13e9ea81e9444dc9f519
cannse <sean.m.cann.17@dartmouth.edu>
2016-10-16 16:39:28 -0400

    date filter prototype

--- frontend/src/components/date_filter.js
+++ frontend/src/components/date_filter.js
@@ -1,10 +1,23 @@
 // date_filter.js
 import React from 'react';
+require('rc-slider/assets/index.css');
+import Rcslider from 'rc-slider';
+// var Rcslider = require('rc-slider');
+
+var labelIndex = [0,1,2,3,4,5,6,7]
+var labelTitle = ["zero","one","one","one","one","one","one"]
+
+var obj = { 0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h' };
+
+function log(value) {
+  console.log(value);
+}
 
 const DateFilter = (props) => {
   return (
-    <div>I let you filter by date.</div>
+    <Rcslider marks={{0:'today',1:'monday',2:'tuesday',3:'wednesday',4:'thursday',5:'friday',6:'saturday',7:'next 2 weeks'}} min={0} max={7} allowCross={false} range dots step={1} defaultValue={[0, 4]} onAfterChange={log} />
   );
 };
+
 
 export default DateFilter;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -52,6 +52,79 @@ class App extends Component {
       </div>
     );
   }
-}
+};
+
+// const DynamicBounds = React.createClass({
+//   getInitialState() {
+//     return {
+//       min: 0,
+//       max: 100,
+//     };
+//   },
+//   onSliderChange(value) {
+//     log(value);
+//   },
+//   onMinChange(e) {
+//     this.setState({
+//       min: +e.target.value || 0,
+//     });
+//   },
+//   onMaxChange(e) {
+//     this.setState({
+//       max: +e.target.value || 100,
+//     });
+//   },
+//   render() {
+//     return (
+//       <div>
+//         <label>Min: </label>
+//         <input type="number" value={this.state.min} onChange={this.onMinChange} />
+//         <br />
+//         <label>Max: </label>
+//         <input type="number" value={this.state.max} onChange={this.onMaxChange} />
+//         <br /><br />
+//         <Slider range defaultValue={[20, 50]} min={this.state.min} max={this.state.max}
+//           onChange={this.onSliderChange} />
+//       </div>
+//     );
+//   },
+// });
+
+// ReactDOM.render(
+//   <div>
+//     <div style={style}>
+//       <p>Basic Range，`allowCross=false`</p>
+//       <Slider range allowCross={false} defaultValue={[0, 20]} onChange={log} />
+//     </div>
+//     <div style={style}>
+//       <p>Basic Range，`step=20` </p>
+//       <Slider range step={20} defaultValue={[20, 20]} onBeforeChange={log} />
+//     </div>
+//     <div style={style}>
+//       <p>Basic Range，`step=20, dots` </p>
+//       <Slider range dots step={20} defaultValue={[20, 40]} onAfterChange={log} />
+//     </div>
+//     <div style={style}>
+//       <p>Controlled Range</p>
+//       <Slider range value={[20, 40]} />
+//     </div>
+//     <div style={style}>
+//       <p>Multi Range</p>
+//       <Slider range={3} value={[20, 40, 60, 80]} />
+//     </div>
+//     <div style={style}>
+//       <p>Customized Range</p>
+//       <CustomizedRange />
+//     </div>
+//     <div style={style}>
+//       <p>Range with dynamic `max` `min`</p>
+//       <DynamicBounds />
+//     </div>
+//   </div>
+//   , document.getElementById('main'));
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('main'));
+// React.render(<App defaultValue={[0, 100]} withBars />, document.body);
