3773e899a1898e92f0d8a3338e4a212547bf284b
Eric Chen <eric.t.chen.17@dartmouth.edu>
2016-10-25 17:37:16 -0400

    Fixing nav_bar after merge conflicts

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -3,14 +3,10 @@ import React from 'react';
 
 const NavBar = (props) => {
   return (
-<<<<<<< HEAD
-    <div id="nav-bar">DartMap</div>
-=======
     <div id="nav-bar">
       <h1 className="app-name">dartmap</h1>
       <button type="button" onClick={props.toggleAddEvent}>Add Event</button>
     </div>
->>>>>>> 55976a05afd80677c67011d5f984f1df1e7c7311
   );
 };
 
