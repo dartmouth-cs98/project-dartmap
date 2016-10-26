2d0163504a133b97735aa9f195e63d94cc149491
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-26 00:18:50 -0400

    fixing some style/code issues with in the index file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom';
 // add the style sheet onto the page
 import './style.scss';
 
-//import the API functions
+// import the API functions
 import postNewEvent from './dartmap-api';
 
 // import the react Components
@@ -32,7 +32,7 @@ class App extends Component {
           location: 1,
           lat: 43.702732,
           lng: -72.290032,
-          description: 'description1'
+          description: 'description1',
         },
         {
           id: '2',
@@ -61,37 +61,7 @@ class App extends Component {
     this.toggleAddEvent = this.toggleAddEvent.bind(this);
   }
 
-  /* 
-   * creates the date data object based on today's date
-   * date data object: {0: Date(), 1: today.getDate()+1, ..., 6: today.getDate()+6, 7: today.getDate()+14}
-   */
-  createDateData() {
-    var today = new Date();
-    var obj = {};
-    var i;
-    // iterate over the week and add in each day
-    for (i = 0; i < 7; i++) {
-      var newDate = new Date();
-      newDate.setDate(today.getDate()+i);
-      obj[i] = newDate;
-    }
-    // add in the day that is two weeks from now
-    var newDate = new Date();
-    newDate.setDate(today.getDate()+14);
-    obj[7] = newDate;
-    return obj;
-  }
-  handleAddEventData(data) {
-    console.log('data from add-event dialog:');
-    console.log(data);
-    postNewEvent(data);
-    this.setState({ addEvent: false });
-  }
-  toggleAddEvent() {
-    this.setState({ addEvent: true });
-  }
-
-  /* 
+  /*
    * creates the date data object based on today's date
    * date data object: {0: Date(), 1: today.getDate()+1, ..., 6: today.getDate()+6, 7: today.getDate()+14}
    */
@@ -121,14 +91,14 @@ class App extends Component {
     this.setState({ addEvent: true });
   }
   showBalloon(eventId) {
-    this.setState({showBalloonEventId: eventId});
+    this.setState({ showBalloonEventId: eventId });
   }
   showStickyBalloon(eventId) {
-    this.setState({showStickyBalloonEventId: eventId});
+    this.setState({ showStickyBalloonEventId: eventId });
 
     // Reset the state so that the popup is a onetime popup.
     setTimeout(function() {
-      this.setState({showStickyBalloonEventId: null});
+      this.setState({ showStickyBalloonEventId: null });
     }.bind(this), 1000);
   }
   render() {
@@ -136,10 +106,12 @@ class App extends Component {
       <div>
         <NavBar toggleAddEvent={this.toggleAddEvent} />
         <MapContainer events={this.state.eventList}
-            showBalloonEventId={this.state.showBalloonEventId}
-            showStickyBalloonEventId={this.state.showStickyBalloonEventId} />
+          showBalloonEventId={this.state.showBalloonEventId}
+          showStickyBalloonEventId={this.state.showStickyBalloonEventId}
+        />
         <EventList events={this.state.eventList} selectedLocation={this.state.selectedLocation}
-            showBalloon={this.showBalloon} showStickyBalloon={this.showStickyBalloon}/>
+          showBalloon={this.showBalloon} showStickyBalloon={this.showStickyBalloon}
+        />
         <FilterContainer onApplyFilter={filters => this.setState({ filters })} dateBarData={this.dateBarData} timeBarData={this.timeBarData} />
         <AddEventDialog addEvent={this.state.addEvent} handleAddEventData={this.handleAddEventData} />
       </div>
