ba0e826257f50a957e7bd4915f00d722ad708840
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-26 01:47:41 -0400

    fixing the event list and event list item tests

--- src/components/__tests__/event_list.test.js
+++ src/components/__tests__/event_list.test.js
@@ -3,10 +3,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../event_list';
+import FakeAppStateData from '../../__mocks__/fake_app_state_data';
 
-it('EventList renders without errors', () => {
-  // Render an EventList
-  const shallowPrototype = shallow(
-    <EventList />
-  );
+describe('EventList', () => {
+  it('renders without errors when given a list of events', () => {
+    // Render an EventList
+    const shallowPrototype = shallow(
+      <EventList events={FakeAppStateData.eventList} />
+    );
+  });
 });
+
--- src/components/__tests__/event_list_item.test.js
+++ src/components/__tests__/event_list_item.test.js
@@ -3,10 +3,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import EventListItem from '../event_list_item';
+import FakeAppStateData from '../../__mocks__/fake_app_state_data';
 
 it('EventListItem renders without errors', () => {
   // Render an EventListItem
   const eListItem = shallow(
-    <EventListItem />
+    <EventListItem event={FakeAppStateData.eventList[0]} />
   );
 });
