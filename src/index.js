0b5c65ffc08089ef1068254e0a2688e571ace3f6
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-30 15:50:31 -0400

    cleaning up the src/index.js code

--- src/index.js
+++ src/index.js
@@ -26,32 +26,7 @@ class App extends Component {
     this.state = {
       filters: null,
       addEvent: false,
-      eventList: [
-        {
-          id: '1',
-          name: 'test event 1',
-          location: 1,
-          lat: 43.702732,
-          lng: -72.290032,
-          description: 'description1',
-        },
-        {
-          id: '2',
-          name: 'test event 2',
-          location: 2,
-          lat: 43.704252,
-          lng: -72.294903,
-          description: 'description2',
-        },
-        {
-          id: '3',
-          name: 'test event 3',
-          location: 3,
-          lat: 43.702141,
-          lng: -72.282574,
-          description: 'description3',
-        },
-      ],  // the filtered list of events received from the back-end
+      eventList: [],  // the filtered list of events received from the back-end
       selectedLocation: null,
       showBalloonEventId: null,
       showStickyBalloonEventId: null,
@@ -60,6 +35,8 @@ class App extends Component {
     this.showBalloon = this.showBalloon.bind(this);
     this.showStickyBalloon = this.showStickyBalloon.bind(this);
     this.toggleAddEvent = this.toggleAddEvent.bind(this);
+  }
+  componentDidMount(){
     getAllEvents((eventList) => { this.setState({ eventList }); });
   }
   handleAddEventData(data) {
