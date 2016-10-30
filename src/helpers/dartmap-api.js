4f20ca4d20b1cd8f471c1b0bb969a4a966654a02
Danielle Midulla <danielle.midulla@gmail.com>
2016-10-30 15:49:20 -0400

    creating a function to reformat the API data and creating temporary tests to try it without a working API endpoint

--- src/helpers/dartmap-api.js
+++ src/helpers/dartmap-api.js
@@ -5,6 +5,66 @@ import $ from 'jquery';
 const API_URL = 'https://dartmapapi.herokuapp.com/api/';
 const EVENT_URL = 'events/';
 
+
+const tempAPIData = [
+  {
+    id: '1',
+    name: 'test event 1',
+    location: {
+      id: 1,
+      name: 'collis',
+      lat: 43.702732,
+      long: -72.290032,
+    },
+    description: 'description1',
+  },
+  {
+    id: '2',
+    name: 'test event 2',
+    location: {
+      id: 2,
+      lat: 43.704252,
+      long: -72.294903,
+    },
+    description: 'description2',
+  },
+  {
+    id: '3',
+    name: 'test event 3',
+    location: {
+      id: 3,
+      lat: 43.702141,
+      long: -72.282574,
+    },
+    description: 'description3',
+  },
+];
+
+/**
+ * formatAPIEventData() returns an event formatted to work with the front-end
+ *
+ * @param {Object} the event object returned by the API
+ * @return {Object} the flattened event object
+ */
+function formatAPIEventData(event) {
+  const newEvent = {};
+  // event data
+  newEvent.name = event.name;
+  newEvent.id = event.id;
+  newEvent.description = event.description;
+  newEvent.location_string = event.location_string;
+  newEvent.date = event.date;
+  newEvent.start_time = event.start_time;
+  newEvent.end_time = event.end_time;
+  // location data
+  newEvent.location_id = event.location.id;
+  newEvent.lat = event.location.lat;
+  newEvent.lng = event.location.long;
+  newEvent.location_name = event.location.name;
+
+  return newEvent;
+}
+
 export function postNewEvent(eventData) {
   const fullUrl = API_URL.concat(EVENT_URL);
   const response = $.ajax({
@@ -25,21 +85,29 @@ export function postNewEvent(eventData) {
 }
 
 export function getAllEvents(saveEventList) {
-  const fullUrl = API_URL.concat(EVENT_URL);
-  $.ajax({
-    url: fullUrl,
-    type: 'GET',
-    dataType: 'json',
-    success: (data) => {
-      console.log(' /events GET was successful! ');
-      console.log(data.results);
-      saveEventList(data.results);
-      return;
-      // return data.results;
-    },
-    error: (xhr, status, err) => {
-      console.log(' /events GET was not successful.');
-      console.error(fullUrl, status, err);
-    },
+  // const fullUrl = API_URL.concat(EVENT_URL);
+  // $.ajax({
+  //   url: fullUrl,
+  //   type: 'GET',
+  //   dataType: 'json',
+  //   success: (data) => {
+  //     console.log(' /events GET was successful! ');
+  //     console.log(data.results);
+  //     const eventList = data.results.map((event) => {
+  //       return formatAPIEventData(event);
+  //     });
+  //     return saveEventList(eventList);
+  //     // return data.results;
+  //   },
+  //   error: (xhr, status, err) => {
+  //     console.log(' /events GET was not successful.');
+  //     console.error(fullUrl, status, err);
+  //   },
+  // });
+  // The next two lines are used to test formatAPIEventData
+  // TODO: remove them once the API GET function works
+  const eventList = tempAPIData.map((event) => {
+    return formatAPIEventData(event);
   });
+  return saveEventList(eventList);
 }
