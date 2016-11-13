4e8373cc0926dd1d23dda1d92839ccd214686515
Edrei Chua <edreichua@hotmail.com>
2016-11-13 17:17:42 -0500

    add dartmap icon

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -4,6 +4,7 @@ import React from 'react';
 const NavBar = (props) => {
   return (
     <div id="nav-bar">
+      <img src="./../../images/dartmap.png" height="60" width="60"/>
       <h1 className="app-name">dartmap</h1>
       <button type="button" onClick={props.toggleAddEvent}>Add Event</button>
     </div>
