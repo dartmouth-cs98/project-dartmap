0c76b79fa0cd392bba778e396ae99e5d80dc9b25
Danielle Midulla <danielle.midulla@gmail.com>
2017-01-24 17:39:55 -0500

    initial google places implementation in the add event dialog

--- src/components/add_events/add_event_page_3.js
+++ src/components/add_events/add_event_page_3.js
@@ -1,6 +1,7 @@
 // add_event_page_3.js
 import React, { Component } from 'react';
-import MapContainer from '../map_container';
+// import MapContainer from '../map_container';
+// import { nearbyMapSearch, textSearch, autoCompleteSearch } from '../../helpers/google-places-api';
 
 
 class AddEventPage3 extends Component {
@@ -10,7 +11,9 @@ class AddEventPage3 extends Component {
     this.state = {
       location_obj: props.data.location_obj,
       location_string: props.data.location_string,
-      center: [43.703337, -72.288578],
+      loc_tmp: null,
+      c: [43.703337, -72.288578],
+      center: { lat: 43.703337, lng: -72.288578 },
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleBack = this.handleBack.bind(this);
@@ -19,6 +22,9 @@ class AddEventPage3 extends Component {
     this.visibleErrorMessages = ['location', 'room'].map((data) => {
       return <div key={data} className="error-msg"> The {data} of the event is required. </div>;
     });
+    this.map = null;
+    this.gPlaces = null;
+    this.gMaps = null;
   }
 
   handleBack(event) {
@@ -44,23 +50,46 @@ class AddEventPage3 extends Component {
 
   handleSelectedLocation(data) {
     this.setState(data);
+    console.log(data);
+  }
+
+  componentDidMount() {
+    const mapHTML = document.getElementById('add-event-map');
+    const searchHTML = document.getElementById('map-search-box');
+    this.gMaps = (window.google && window.google.maps);
+    this.map = new this.gMaps.Map(mapHTML, { center: this.state.center, zoom: 15 });
+    this.map.addListener('click', (event) => {
+      console.log(event.latLng.lat(), event.latLng.lng());
+    });
+    this.gPlaces = new this.gMaps.places.PlacesService(this.map);
+    this.gPlaces.nearbySearch({ location: this.state.center, radius: 100 }, (result) => { console.log(result); });
+    this.textBox = new this.gMaps.places.Autocomplete(searchHTML);
+    this.textBox.bindTo('bounds', this.map);
+    this.textBox.addListener('place_changed', (event) => {
+      console.log(this.textBox.getPlace());
+    });
+    console.log(this.map, this.gPlaces);
   }
 
   render() {
     // const locationErrorMessage = (this.state.location === '') ? this.visibleErrorMessages[0] : this.hiddenErrorMessage;
     const roomErrorMessage = (this.state.location_string === '') ? this.visibleErrorMessages[1] : this.hiddenErrorMessage;
-    const mapHeight = '300px';
-    const mapWidth = '300px';
+    // const mapHeight = '300px';
+    // const mapWidth = '300px';
     return (
       <form className="add-event-form" onSubmit={this.handleSubmit}>
         <div className="add-event-fields">
-          <MapContainer events={this.state.location_obj || []}
-            showBalloonEventId={this.nullFunction}
-            showStickyBalloonEventId={this.nullFunction}
-            height={mapHeight}
-            width={mapWidth}
-            handleSelectedLocation={this.handleSelectedLocation}
-            center={this.state.center}
+          <div id="add-event-map" />
+          <h2>Location String TEMP!!!:*</h2>
+          <input
+            id="map-search-box"
+            type="text"
+            placeholder="e.g. hi"
+            value={this.state.loc_tmp || ''}
+            onChange={(event) => {
+              this.setState({ loc_tmp: event.target.value });
+            }}
+            className={(this.state.loc_tmp !== '') ? 'add-event-text add-event-loc-string' : 'add-event-text add-event-loc-string error-box'}
           />
           <h2>Location Name to Display:*</h2>
           <input
@@ -91,3 +120,12 @@ class AddEventPage3 extends Component {
 }
 
 export default AddEventPage3;
+
+// <MapContainer events={this.state.location_obj || []}
+//   showBalloonEventId={this.nullFunction}
+//   showStickyBalloonEventId={this.nullFunction}
+//   height={mapHeight}
+//   width={mapWidth}
+//   handleSelectedLocation={this.handleSelectedLocation}
+//   center={this.state.center}
+// />
